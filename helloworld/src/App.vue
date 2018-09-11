3b2e24edcb018df7c7673f58d392b1904828c270
Sergio Tomas <sergio.tomas@globalhitss.com.br>
2018-09-11 20:18:14 -0300

    - -

--- helloworld/src/App.vue
+++ helloworld/src/App.vue
@@ -1,21 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <p> {{ nome | meuFiltro }}</p>
+    <p>{{ outroNome }}</p>
+
+    <input type="text" v-model="outroNome">
   </div>
 </template>
 
@@ -24,7 +12,25 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      nome: 'Sergio'
+    }
+  },
+  computed: {
+    outroNome: {
+      get: function (){
+        return this.nome.toUpperCase();
+      },
+      set: function(newValue) {
+        this.nome = newValue.substring(0, 3);
+      }
+    }
+  },
+  filters: {
+    meuFiltro(valor) {
+      valor = valor + " - Bem filtrado!";
+
+      return valor;
     }
   }
 }
