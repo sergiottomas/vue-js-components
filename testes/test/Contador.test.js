7128a06b9f4c2b1e1d644b7c59419747fd1d24a7
Sergio Tomas <sergio.tomas@globalhitss.com.br>
2018-10-01 18:53:29 -0300

    - finalizado testes

--- testes/test/Contador.test.js
+++ testes/test/Contador.test.js
@@ -2,11 +2,22 @@ import test from 'ava';
 import Contador from './../src/Contador.vue'
 import { mount } from 'avoriaz'
 
+var component = mount(Contador);
+
 test('Analisar se contador está em 0', t => {
-  t.is(Contador.default.data().total, 0);
+  t.is(component.data().total, 0);
 });
 
 test('subtraindo um número do total', t => {
-  Contador.default.methods.subtrair();
-  t.is(Contador.default.data().total, -1);
+  let button = component.find("button")[0];
+  button.trigger('click');
+
+  t.is(component.data().total, -1);
+});
+
+test('somando um número do total', t => {
+  let button = component.find("button")[1];
+  button.trigger('click');
+
+  t.is(component.data().total, 0);
 });
